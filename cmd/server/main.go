55c3983ad7015de5f6f44b34eb4719619aa0280d
beta <beta.nycz@icloud.com>
2025-01-28 18:06:39 +0100

    feat(lambda): add initial version of Lambda-ready backend

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"context"
 	"go-bot/internal/api"
 	"go-bot/internal/config"
 	"go-bot/internal/db"
 	"os"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
 )
 
-func main() {
-	// set logging level to debug and use console output
+var ginLambda *ginadapter.GinLambda
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return ginLambda.ProxyWithContext(ctx, req)
+}
+
+func init() {
+	// Set logging level to debug and use console output
 	zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
@@ -25,7 +36,16 @@ func main() {
 	router := gin.Default()
 	api.RegisterRoutes(router)
 
-	// start http server on configured port
-	log.Info().Msgf("server running on port %s", cfg.Port)
-	router.Run(":" + cfg.Port)
+	// running in AWS Lambda
+	if os.Getenv("AWS_LAMBDA_RUNTIME_API") != "" {
+		ginLambda = ginadapter.New(router)
+	} else {
+		// running locally
+		log.Info().Msgf("server running on port %s", cfg.Port)
+		router.Run(":" + cfg.Port)
+	}
+}
+
+func main() {
+	lambda.Start(Handler)
 }
